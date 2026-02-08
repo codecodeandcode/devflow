922e68ec51e87e2483b0f4307d9fb672b1380d7b
codecodeandcode <1194220238@qq.com>
2026-02-09 02:58:13 +0800

    设置mongoDB和mongoose与user表

---
+++ lib/mongoose.ts
@@ -0,0 +1,50 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("请在环境变量中设置 MONGODB_URI");
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongooseCache: MongooseCache;
+}
+
+let cache = global.mongooseCache;
+
+if (!cache) {
+  cache = global.mongooseCache = {
+    conn: null,
+    promise: null,
+  };
+}
+
+async function dbConnet(): Promise<Mongoose> {
+  if (cache.conn) {
+    return cache.conn;
+  }
+  if (!cache.promise) {
+    cache.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "devflow",
+      })
+      .then((result) => {
+        console.log("MongoDB 连接成功");
+        return result;
+      })
+      .catch((error) => {
+        console.error("MongoDB 连接失败", error);
+        throw error;
+      });
+  }
+
+  cache.conn = await cache.promise;
+  return cache.conn;
+}
+
+export default dbConnet;
